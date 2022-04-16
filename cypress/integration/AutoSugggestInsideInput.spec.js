bb338b86c739e584f66ab52fe4d0b59af079860b
Andus <anduscheung18@gmail.com>
2022-04-17 03:45:18 +0800

    More cypress tests

--- cypress/integration/AutoSugggestInsideInput.spec.js
+++ cypress/integration/AutoSugggestInsideInput.spec.js
@@ -1,13 +1,39 @@
 /// <reference types="cypress" />
 
 describe("My First Test", () => {
-  it('finds the content "type"', () => {
+  it("search with one chars", () => {
     cy.visit("localhost:3000");
     cy.get("#asi-input").type("w");
     cy.get("#asi-input").should("have.value", "www.google.com");
+  });
 
-    cy.get("#asi-input").type("{rightArrow}");
-    cy.get("#asi-input").type("{backspace}");
+  it("search with multiple chars", () => {
+    cy.visit("localhost:3000");
+    cy.get("#asi-input").type("w").type("w").type("w");
+    cy.get("#asi-input").should("have.value", "www.google.com");
+  });
+
+  it("search with multiple chars and change suggestion", () => {
+    cy.visit("localhost:3000");
+    cy.get("#asi-input").type("w").type("w").type("w").type(".").type("n");
+    cy.get("#asi-input").should("have.value", "www.npmjs.com");
+  });
+
+  it("search and delete from right side", () => {
+    cy.visit("localhost:3000");
+    cy.get("#asi-input").type("w").type("{rightArrow}").type("{backspace}");
     cy.get("#asi-input").should("have.value", "www.google.co");
+
+    cy.get("#asi-input")
+      .type("{leftArrow}")
+      .type("{leftArrow}")
+      .type("{backspace}");
+    cy.get("#asi-input").should("have.value", "www.googleco");
+  });
+
+  it("search and delete from left side", () => {
+    cy.visit("localhost:3000");
+    cy.get("#asi-input").type("w").type("{leftArrow}").type("{del}");
+    cy.get("#asi-input").should("have.value", "ww.google.com");
   });
 });
